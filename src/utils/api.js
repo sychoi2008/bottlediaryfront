ad1877505fb150690342a88aaf00bfb65c418274
narumi <sychoi2008@sookmyung.ac.kr>
2025-04-01 19:05:13 +0900

    chore: remove broken functions/api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 console.log("API BaseURL", process.env.REACT_APP_API_BASE_URL);
 
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API_BASE_URL,
-// });
 const api = axios.create({
-  baseURL: "/api", // 프론트에선 /api 붙여서 요청 → 프록시로 전달됨
+  baseURL: "http://3.39.23.44:8080/",
 });
+// const api = axios.create({
+//   baseURL: "/api", // 프론트에선 /api 붙여서 요청 → 프록시로 전달됨
+// });
 
 export default api;
