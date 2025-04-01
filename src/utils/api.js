0e41366c0c2d586c3233faa76e6f875a0c9a7a1e
Narumi <sychoi2008@sookmyung.ac.kr>
2025-04-01 20:35:30 +0900

    fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,8 @@
 // src/utils/api.js
 import axios from "axios";
 
-console.log("API BaseURL", process.env.REACT_APP_API_BASE_URL);
-
 const api = axios.create({
-  baseURL: "/api", // 프론트에선 /api 붙여서 요청 → 프록시로 전달됨
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
