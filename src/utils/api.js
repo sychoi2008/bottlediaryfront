f6dfdd75c7b811672025c5f13229fcf7c55dc673
narumi <sychoi2008@sookmyung.ac.kr>
2025-04-01 17:31:56 +0900

    trigger redeploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 console.log("API BaseURL", process.env.REACT_APP_API_BASE_URL);
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "/api",
 });
 
 export default api;
